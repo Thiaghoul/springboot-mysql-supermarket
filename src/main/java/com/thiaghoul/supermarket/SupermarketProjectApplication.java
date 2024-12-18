e94170be893cfb7e912732503a8243a9d4cee2d4
thiaghoul <thiagohnunes.w@gmail.com>
2024-12-18 19:54:53 -0300

    project added

---
+++ src/main/java/com/thiaghoul/supermarket/SupermarketProjectApplication.java
@@ -0,0 +1,13 @@
+package com.thiaghoul.supermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiaghoul/supermarket/SupermarketProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiaghoul.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
